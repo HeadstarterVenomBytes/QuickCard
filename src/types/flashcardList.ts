144086733a8df5035723e6f796a106f5206d849d
peppapig450 <nbgenius1@gmail.com>
2024-08-12 15:36:11 -0700

    add additional types relating to containers of flashcards

--- src/types/flashcardList.ts
+++ src/types/flashcardList.ts
@@ -1,3 +1,10 @@
 import { Flashcard } from "./flashcard";
 
 export type FlashcardList = Flashcard[];
+
+export interface FlashcardSet {
+  name: string;
+  flashcards: FlashcardList;
+}
+
+export type FlashcardSetList = FlashcardSet[];
