901d0cf3e2ff177febbeddbdf5f12be6290a6948
peppapig450 <nbgenius1@gmail.com>
2024-08-13 15:50:32 -0700

    change the directory path for the sign-in page for clerk

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import SignInNavigationBar from "../components/SignInPage/NavigationBar";
-import SignInPage from "../components/SignInPage/SignInPage";
+import SignInNavigationBar from "@/app/components/SignInPage/NavigationBar";
+import SignInPage from "@/app/components/SignInPage/SignInPage";
 
 export default function SignUpPage(): React.JSX.Element {
   return (
