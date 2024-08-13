24bec828b8177bb4be52d9a6a9baf6789c411b1f
peppapig450 <nbgenius1@gmail.com>
2024-08-12 21:04:26 -0700

    implement the components in a page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInNavigationBar from "../components/SignInPage/NavigationBar";
+import SignInPage from "../components/SignInPage/SignInPage";
+
+export default function SignUpPage(): React.JSX.Element {
+  return (
+    <>
+      <SignInNavigationBar />
+      <SignInPage />
+    </>
+  );
+}
