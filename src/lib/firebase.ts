a71a1e841e037c60eeed5010050cdb7389618cd3
peppapig450 <nbgenius1@gmail.com>
2024-08-14 12:53:48 -0700

    initialize and export auth as well for firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "@/config/firebaseConfig";
+import { getAuth } from "firebase/auth";
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export default db;
+export const db = getFirestore(app);
+export const auth = getAuth(app);
