ce35af4d67938b973f6ca59016d61d4f77fa2ef1
peppapig450 <nbgenius1@gmail.com>
2024-08-14 14:48:22 -0700

    use the hook for the flashcard set display page

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,42 +1,36 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useUser } from "@clerk/nextjs";
 import { AnyFlashcard, FlashcardSetList } from "@/types/flashcard-types";
 import { useRouter } from "next/router";
-import { getFlashcardSets } from "@/utils/getFlashcards";
+import { useFlashcardSets } from "@/hooks/useFlashcardSets";
 import FlashcardSetsOverviewGrid from "../components/FlashcardSetsOverviewGrid";
 import TypographyHeader from "../components/TypographyHeader";
 import Container from "@mui/material/Container";
 
 // TODO: maybe use `React.FC<type stuff>` for stricter typing
 export default function Flashcard(): React.JSX.Element {
-  const { isLoaded, isSignedIn, user } = useUser();
-  const [flashcardSets, setFlashcardSets] = useState<
-    FlashcardSetList<AnyFlashcard>
-  >([]);
   const router = useRouter();
-
-  useEffect(() => {
-    async function fetchFlashcards() {
-      if (isLoaded && isSignedIn) {
-        const sets = await getFlashcardSets(user);
-        setFlashcardSets(sets);
-      }
-    }
-
-    fetchFlashcards();
-  }, [isLoaded, isSignedIn, user]);
+  const { flashcardSets, isLoading, error } = useFlashcardSets();
 
   const handleSetClick = (id: string) => {
     router.push(`/flashcard?id=${id}`);
   };
 
+  // TODO: Style all of these with material UI
+  if (isLoading) {
+    return <div>Loading flashcards...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <Container maxWidth="md">
       <TypographyHeader title="Your Flashcard Sets" />
       <FlashcardSetsOverviewGrid
-        flashcardSets={flashcardSets}
+        flashcardSets={flashcardSets || []}
         onSetClick={handleSetClick}
       />
     </Container>
