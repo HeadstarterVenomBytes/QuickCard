2b47f8c606e176bb0d17021d19263f3b3093d8e0
peppapig450 <nbgenius1@gmail.com>
2024-08-15 15:26:20 -0700

    change the generate route.ts to use the utils function

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,56 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { OpenAI } from "openai";
+import { FlashcardFormData } from "@/types/flashcard-form-types";
 import { Flashcard, FlashcardList } from "@/types/flashcard-types";
-
-// TODO: switch this to use LangChain for prompts
-const systemPrompt = `
-You are a flashcard creator. Your task is to take in text and create exactly 10 flashcards from it. Both the front and back of each card should be one sentence long. 
-
-Your response must consist solely of a JSON object in the following format, with no additional text before or after:
-
-{
-  "flashcards": [
-    {
-      "front": "Front of card 1",
-      "back": "Back of card 1"
-    },
-    {
-      "front": "Front of card 2",
-      "back": "Back of card 2"
-    },
-    ...
-  ]
-}
-
-Ensure that your response contains exactly 10 flashcard objects within the "flashcards" array.
-`;
-
-const client = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.OPENROUTER_API_KEY as string,
-});
+import { generateFlashcards } from "@/utils/generateFlashcardData";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  const data = await req.text();
+  try {
+    const formData: FlashcardFormData = await req.json();
 
-  // Define the conversation structure
-  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
-    { role: "system", content: systemPrompt },
-    { role: "user", content: data },
-  ];
+    const flashcardList: FlashcardList<Flashcard> = await generateFlashcards(
+      formData
+    );
 
-  // Create the OpenAI API request
-  const completion = await client.chat.completions.create({
-    messages: messages,
-    model: "meta-llama/llama-3.1-8b-instruct:free",
-    response_format: { type: "json_object" }, // TODO: schema instead?
-  });
-
-  // Parse and extract flashcards
-  const flashcardsData: { flashcards: FlashcardList<Flashcard> } = JSON.parse(
-    completion.choices[0]?.message?.content as string
-  );
-
-  // Return the flashcards as a JSON response
-  return NextResponse.json(flashcardsData.flashcards);
+    return NextResponse.json(flashcardList);
+  } catch (error) {
+    console.error("Error generating flashcards:", error);
+    return NextResponse.json(
+      { error: "Error generating flashcards" },
+      { status: 500 }
+    );
+  }
 }
