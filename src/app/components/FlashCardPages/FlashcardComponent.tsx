bcb113f35f8c57c2f805dd5896783a6ecea48c1f
peppapig450 <nbgenius1@gmail.com>
2024-08-16 11:57:53 -0700

    get flashcard flipping working

--- src/app/components/FlashCardPages/FlashcardComponent.tsx
+++ src/app/components/FlashCardPages/FlashcardComponent.tsx
@@ -16,48 +16,50 @@ interface FlashcardComponentProps {
   setId: string | null;
 }
 
-
-
 const FlashcardComponent: React.FC<FlashcardComponentProps> = ({
   flashcard,
   isFlipped,
   onClick,
 }) => {
   return (
-    <Card style={{ 
-      height:"100%",
-      width: "100%",
-      left: "3.21%",
-      right: "3.21%",
-      top: "10.42%",
-      bottom: "10.42%",
-      background: "#003050",
-      boxShadow: "4px 4px 9px rgba(0, 0, 0, 0.25)",
-      borderRadius: "20px"
-     }}>
+    <Card
+      style={{
+        background: "#003050",
+        boxShadow: "4px 4px 9px rgba(0, 0, 0, 0.25)",
+        borderRadius: "20px",
+      }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "auto",
+        width: "100%",
+        minHeight: 150,
+        maxHeight: "100%",
+      }}
+    >
       <CardActionArea onClick={onClick}>
-        <CardContent>
+        <CardContent sx={{ flexGrow: 1 }}>
           <Box sx={{ perspective: 1000 }}>
             <Box
-              // sx={{
-              //   transformStyle: "preserve-3d",
-              //   transition: "transform 0.6s",
-              //   transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
-              // }}
+              sx={{
+                transformStyle: "preserve-3d",
+                transition: "transform 0.6s",
+                transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
+              }}
             >
               <Box
                 sx={{
                   position: "absolute",
                   width: "100%",
                   backfaceVisibility: "hidden",
-                  // transform: "rotateY(0deg)",
+                  transform: "rotateY(0deg)",
                 }}
               >
                 <Typography variant="h5" component="div" color="#FFFFFF">
                   {flashcard?.front}
                 </Typography>
               </Box>
-              {/* <Box
+              <Box
                 sx={{
                   position: "absolute",
                   width: "100%",
@@ -68,7 +70,7 @@ const FlashcardComponent: React.FC<FlashcardComponentProps> = ({
                 <Typography variant="h5" component="div" color="#FFFFFF">
                   {flashcard?.back}
                 </Typography>
-              </Box> */}
+              </Box>
             </Box>
           </Box>
         </CardContent>
