38b3b3f5da6a0356c50363c2d785da6cf7706302
peppapig450 <nbgenius1@gmail.com>
2024-08-16 14:06:51 -0700

    enable app router cache and css baseline for improved material ui performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { FirebaseAuthProvider } from "./context/FirebaseAuthContext";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const metadata: Metadata = {
   title: "QuickCard",
@@ -17,11 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" style={{ height: "100%" }}>
+    <html lang="en">
       <ClerkProvider>
         <FirebaseAuthProvider>
-          <body className={inter.className} style={{ height: "100%" }}>
-            <main style={{ height: "100%" }}>{children}</main>
+          <body>
+            <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+              <CssBaseline />
+              {children}
+            </AppRouterCacheProvider>
           </body>
         </FirebaseAuthProvider>
       </ClerkProvider>
