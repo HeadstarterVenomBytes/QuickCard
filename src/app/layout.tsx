d86294798d2d89ec3446fe87670968e34a2ea396
peppapig450 <nbgenius1@gmail.com>
2024-08-16 19:03:03 -0700

    add vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { FirebaseAuthProvider } from "./context/FirebaseAuthContext";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import theme from "@/styles/theme";
 
 export const metadata: Metadata = {
@@ -25,6 +27,8 @@ export default function RootLayout({
               <ThemeProvider theme={theme}>
                 <CssBaseline />
                 {children}
+                <Analytics />
+                <SpeedInsights />
               </ThemeProvider>
             </AppRouterCacheProvider>
           </body>
