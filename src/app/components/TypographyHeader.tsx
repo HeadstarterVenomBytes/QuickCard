e1cad16e6cbd9e285a43666e2459298029aa267f
peppapig450 <nbgenius1@gmail.com>
2024-08-16 18:44:27 -0700

    allow passing a color to the TypographyHeader component

--- src/app/components/TypographyHeader.tsx
+++ src/app/components/TypographyHeader.tsx
@@ -6,18 +6,21 @@ interface TypographyHeaderProps
   title: string;
   variant?: TypographyProps["variant"];
   component?: TypographyProps["component"];
+  color?: TypographyProps["color"];
 }
 
 const TypographyHeader: React.FC<TypographyHeaderProps> = ({
   title,
   variant = "h4",
   component = "h4",
+  color,
   ...otherProps
 }) => (
   <Typography
     variant={variant}
     component={component}
     gutterBottom
+    color={color}
     {...otherProps}
   >
     {title}
